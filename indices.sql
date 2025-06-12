9e8b822b5ce23f4066bbe2501fe798f33a2da720
Recamm <camiloreartesgm@gmail.com>
2025-06-12 11:12:56 -0300

    Indices

--- indices.sql
+++ indices.sql
@@ -5,4 +5,7 @@ create index idProducto_Publicacion on Publicacion(idProducto);
 create index nombreProducto on Producto(nombre);
 
 -- 2
-create index 
+create index correoElectronico on Usuario(email);
+
+-- 3
+create index estados on Publicacion(estado);
